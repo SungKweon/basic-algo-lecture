384470bc8e516958fd1131b9056b482ffe9ae2be
Funny_Sung <63580114+SungKweon@users.noreply.github.com>
2021-07-28 17:33:54 +0900

    바킹독님! 질문 드립니다!
    안녕하세요! 바킹독님! 
    깃헙, 백준 초보라서..질문드립니다! (깃헙에서 이렇게 질문드리는 게 맞는지도 모르겠네요..ㅎㅎ)
    코테 준비하는 겸, '집단지성 프로젝트'에 참여하고 있는데용 
    
    문제 10807에 대한 코드가 vscode에서는 잘 돌아가는데, 
    백준에 제출했을 때는, 채점중 30%에서 틀렸다고 뜨네요 ㅠㅠ
    왜 이런 현상이 뜨는지 답변 줄수 있으신가요?
    
    백준 제출 링크: https://www.acmicpc.net/source/31550840

--- 0x03/solutions/10807.cpp
+++ 0x03/solutions/10807.cpp
@@ -4,8 +4,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int freq[205]={};
+int arr[105]={};//크기 주의
+
 int main(void){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int cnt;
+    int v;
+    cin >> cnt;
+
+    for (int i = 0; i < cnt; i++)
+    {
+        cin >> arr[i];
+    }   
+    cin >> v;
+
+    for(auto c : arr)
+        freq[c+100]++;
+     
+    cout << freq[v+100];    
+    return 0;
+    
 }
